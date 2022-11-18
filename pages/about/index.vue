b60bdfc2f1d9fa9ae5c822123ed0c7b88b056652
You Know <111015172+OptimisticImpulse@users.noreply.github.com>
2022-11-17 19:26:02 -0500

    Create index.vue

---
+++ pages/about/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+  <NuxtLink to="/">Home</NuxtLink><br>
+  <NuxtLink to="/about">About</NuxtLink><br>
+This is the about page on netlify.
+
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
